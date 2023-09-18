92c3015cd4587a52d293a5e51d2ef83717257286
pasitk <pasit.kongkunakornkul@thomsonreuters.com>
2023-09-18 19:05:01 +0700

    Add title

--- docs/_app/immutable/chunks/paths.56b863f9.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_10f1bxv)==null?void 0:s.base)??"/pasitk.github.io";var t;const e=((t=globalThis.__sveltekit_10f1bxv)==null?void 0:t.assets)??a;export{e as a,a as b};
