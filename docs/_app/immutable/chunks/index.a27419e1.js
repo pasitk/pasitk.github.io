9212f2a18707f5efc1594f8c90f0f7d41e458510
pasitk <pasit.kongkunakornkul@thomsonreuters.com>
2023-09-18 13:10:57 +0700

    Fix image preloading 2

--- docs/_app/immutable/chunks/index.a27419e1.js
+++
@@ -1,6 +0,0 @@
-import{U as p}from"./index.12de889b.js";function $(n){const t=n-1;return t*t*t+1}function U(n,{delay:t=0,duration:a=400,easing:e=$,x:f=0,y:i=0,opacity:s=0}={}){const o=getComputedStyle(n),c=+o.opacity,y=o.transform==="none"?"":o.transform,m=c*(1-s),[u,r]=p(f),[d,_]=p(i);return{delay:t,duration:a,easing:e,css:(l,g)=>`
-			transform: ${y} translate(${(1-l)*u}${r}, ${(1-l)*d}${_});
-			opacity: ${c-m*g}`}}function C(n,{delay:t=0,duration:a=400,easing:e=$,start:f=0,opacity:i=0}={}){const s=getComputedStyle(n),o=+s.opacity,c=s.transform==="none"?"":s.transform,y=1-f,m=o*(1-i);return{delay:t,duration:a,easing:e,css:(u,r)=>`
-			transform: ${c} scale(${1-y*r});
-			opacity: ${o-m*r}
-		`}}export{U as f,C as s};
