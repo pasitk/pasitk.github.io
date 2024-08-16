0b20af2f1cc772a190a1dcd2f587759a4e41e13f
pasitk <pasit.kongkunakornkul@thomsonreuters.com>
2024-08-17 01:47:10 +0700

    Add Google Analytics Tag

--- docs/_app/immutable/chunks/paths.62f66173.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_323d0e)==null?void 0:s.base)??"/pasitk.github.io";var e;const a=((e=globalThis.__sveltekit_323d0e)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ docs/_app/immutable/chunks/paths.c80e9a00.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1e608oi)==null?void 0:s.base)??"/pasitk.github.io";var e;const a=((e=globalThis.__sveltekit_1e608oi)==null?void 0:e.assets)??t;export{a,t as b};
